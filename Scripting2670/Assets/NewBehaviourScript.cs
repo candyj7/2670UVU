3cfca1412b768bff461be843a003b9471a593b85
candyj7 <singingcandella@gmail.com>
2017-08-25 14:09:03 -0600

    Smee stop me smee smee I'm Committing

---
+++ Scripting2670/Assets/NewBehaviourScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class MoveInput : MonoBehaviour
+{
+
+    Action KeyAction;
+
+    private void Start()
+    {
+        KeyAction = Move;
+    }
+
+    //Use this for initialization
+    void Move () {
+        print("Left Arrow");
+    }
+
+    //Update is called once per frame
+    void Update() {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+        KeyAction();
+        }
+    }
+}
