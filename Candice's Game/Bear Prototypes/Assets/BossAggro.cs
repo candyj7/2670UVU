aa015d089bd05b3aaac1f0f7185db66c46bce28b
candyj7 <singingcandella@gmail.com>
2017-12-13 14:04:13 -0700

    Next

---
+++ Candice's Game/Bear Prototypes/Assets/BossAggro.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class BossAggro : MonoBehaviour {
+
+	public static Action<Transform> passPlayer;
+	public static Action endAggro;
+	public Transform player;
+
+	void OnTriggerEnter(Collider other)
+	{
+		player = other.transform;
+		passPlayer (player);
+	}
+
+	void OnTriggerExit()
+	{
+		endAggro ();
+	}
+
+}
---
+++ Candice's Game/Bear Prototypes/Assets/bossMotor.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bossMotor : MonoBehaviour {
+
+	public Rigidbody bossRB;
+	public Transform target;
+
+	public float waitTime_1;
+	public float waitTime_2;
+	public float jumpHeight;
+	public float moveSpeed;
+	public float playerDir;
+
+	public bool isAggro;
+
+	// Use this for initialization
+	void Start () 
+	{
+		bossRB = GetComponent<Rigidbody> ();
+		isAggro = false;
+		BossAggro.passPlayer += BossIsAggro;
+		BossAggro.endAggro = BossNotAggro;
+	}
+		
+	// Update is called once per frame
+	void Update () 
+	{
+		if (target != null) 
+		{
+			float playerDist = this.transform.position.x - target.position.x;
+
+			if (playerDist > 0) {
+				playerDir = -1;
+			} 
+			else if (playerDist < 0) 
+			{
+				playerDir = 1;
+			}
+		}	
+	}
+	IEnumerator BossMovement()
+	{
+		if (isAggro) {
+			bossRB.velocity = new Vector3 (playerDir * moveSpeed * Time.deltaTime, jumpHeight * Time.deltaTime, 0); 
+		} 
+		else 
+		{
+			yield break;
+		}
+
+		yield return new WaitForSeconds (waitTime_1);
+
+		if (isAggro) {
+			bossRB.velocity = new Vector3 (playerDir * moveSpeed * Time.deltaTime, jumpHeight * Time.deltaTime, 0); 
+		} 
+		else 
+		{
+			yield break;
+		}
+
+		yield return new WaitForSeconds (waitTime_1);
+
+		if (isAggro) {
+			bossRB.velocity = new Vector3 (playerDir * moveSpeed * Time.deltaTime, jumpHeight * Time.deltaTime, 0); 
+		} 
+		else 
+		{
+			yield break;
+		}
+
+		yield return new WaitForSeconds (waitTime_2);
+
+		if (isAggro) {
+			StartCoroutine (BossMovement ()); 
+		} 
+		else 
+		{
+			yield break;
+		}
+
+
+
+	}//end of Coroutine
+
+	void BossIsAggro (Transform _player)
+	{
+		target = _player;
+		isAggro = true;
+		StartCoroutine (BossMovement ());  
+	}
+
+	void BossNotAggro()
+	{
+		isAggro = false;
+	}
+
+}
